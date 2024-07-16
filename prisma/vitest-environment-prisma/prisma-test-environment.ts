359fc31383eddeea18133359fb95e2ab08fd364c
erik-sig <erik.signori_thalheimer@hotmail.com>
2024-07-16 19:45:23 -0300

    fix errors from github actions

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 
+
 import { execSync } from 'node:child_process';
 import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest';
